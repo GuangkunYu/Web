bca41d052707fc915f731ce103b598c840fafa48
YuGuangkun <1795056480@qq.com>
2021-03-02 19:40:11 +0800

    webpack

---
+++ 7.Vue/vue-webpack/01-webpack起步/src/main.js
@@ -0,0 +1,4 @@
+const {add, mutil} = require('./mathUtils.js');
+
+console.log(add(20, 30));
+console.log(mutil(20, 30));
---
+++ 7.Vue/vue-webpack/01-webpack起步/src/mathUtils.js
@@ -0,0 +1,12 @@
+function add(num1, num2){
+    return num1 + num2
+}
+
+function mutil(num1, num2){
+    return num1 * num2
+}
+
+module.export = {
+    add, 
+    mutil
+}
